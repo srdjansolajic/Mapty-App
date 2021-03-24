556a3c70dd5ab981efe27f35068072d86240d57e
srdjansolajic <solajic.srdjan@gmail.com>
2021-03-24 07:31:29 +0100

    html & css

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// Elements
+
+// ul
+const containerWorkouts = document.querySelector('.workouts');
+
+// form
+const form = document.querySelector('.form');
+
+// form input
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
